6b3efcec26842eb5bb7c47afd5c5bd86b93fb0af
Jesus <jesus.jauregui-c@mail.escuelaing.edu.co>
2025-11-30 22:09:11 -0500

    quality setting # 1 for security implemented

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,6 @@ import logger from "../utils/logger";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI!);
         logger.info('MongoDB connected');
     } catch (error) {
         logger.error({ err: (error as any)?.message || error }, 'MongoDB connection failed');
